c4b8071df86e56f581fc67e1093fd801f703e9d2
Ale2030 <alexiscuevas551@gmail.com>
2024-12-19 00:30:35 -0300

    Agregado de entidades y sus respectivos repositorios.

--- src/main/java/com/alexis/proyecto/biblioteca_api/models/Autor.java
+++ src/main/java/com/alexis/proyecto/biblioteca_api/models/Autor.java
@@ -1,5 +1,39 @@
 package com.alexis.proyecto.biblioteca_api.models;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Clase representa la entidad Autor
+ * Administra a los autores
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "autores")
 public class Autor {
-    
+
+    /* ID unico del Autor */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /* Nombre completo del Autor. */
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    /* Fecha nacimiento del Autor. */
+    private LocalDate fechaNacimiento;
+
+    /* Nacionalidad del Autor. */
+    private String nacionalidad;
+
+    /* Biografia del autor. */
+    private String biografia;
+
 }
--- src/main/java/com/alexis/proyecto/biblioteca_api/models/Libro.java
+++ src/main/java/com/alexis/proyecto/biblioteca_api/models/Libro.java
@@ -1,5 +1,52 @@
 package com.alexis.proyecto.biblioteca_api.models;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Clase representa la entidad Libro
+ * Administra los libros de la aplicacion.
+ * 
+ * @author Alex
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "libros")
 public class Libro {
-    
+
+    /* ID unico del libro. */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idLibro;
+
+    /* Genero del libro. */
+    @Column(nullable = false)
+    private String genero;
+
+    /* Titulo del libro. */
+    @Column(nullable = false)
+    private String titulo;
+
+    /* Entidad de Autor asociada. */
+    @ManyToOne
+    @JoinColumn(name = "id_autor", nullable = true)
+    private Autor autor;
+
+    /* Entidad de Editorial asociada. */
+    @ManyToOne
+    @JoinColumn(name = "id_editorial", nullable = true)
+    private Editorial editorial;
+
+    /* Fecha en que fue publicado el libro. */
+    private LocalDate fechaPublicacion;
+
+    /* Campo para saber si el registro esta activo. */
+    private Boolean activo = true;
+
 }
