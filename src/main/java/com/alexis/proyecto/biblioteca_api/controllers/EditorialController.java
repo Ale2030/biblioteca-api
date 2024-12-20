e2e5d3bc8d9c8754643cead6ed27e06f9be00d63
Ale2030 <alexiscuevas551@gmail.com>
2024-12-20 08:33:28 -0300

    Creacion de las clases Controllers

---
+++ src/main/java/com/alexis/proyecto/biblioteca_api/controllers/EditorialController.java
@@ -0,0 +1,8 @@
+package com.alexis.proyecto.biblioteca_api.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EditorialController {
+    
+}
